f16365269ab100cc7d6fcc44eecb96631a712c14
Syaroful <syaroful10403@gmail.com>
2023-07-19 12:42:55 +0700

    portfolio with tailwind part 1

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./**/*.{html,js}'],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors: {
+        primary: '#4F709C',
+        dark : '#213555',
+        secondary : '#94a3b8',
+        light : '#F5EFE7',
+      },
+      screens: {
+        '2xl': '1320px',
+      }
+    },
+  },
+  plugins: [],
+}
